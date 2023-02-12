2ceeec342e80028433e489563beee258a79988f5
Vasily Petrunin <vasaaa7777@gmail.com>
2023-02-12 18:35:21 +0400

    add Task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+for(double i = 1; i <= n; i = i + 1)
+{
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+}
